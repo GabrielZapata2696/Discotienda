0b7859c06bae5ac23062a04e29213146917e5c36
GabrielZapata2696 <sr.gabrielzm@gmail.com>
2020-11-05 22:58:24 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/mycompany/discotienda/controller/IndexController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.discotienda.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Gabriel Zapata
+ * @author Mary Zapata
+ * @since 05/11/2020
+ * @version 1.0.0
+ */
+
+@Named(value = "indexController")
+@RequestScoped
+public class IndexController {
+
+    /**
+     * Creates a new instance of IndexController
+     */
+    public IndexController() {
+    }
+    
+}
